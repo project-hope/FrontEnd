581602a92bc3c07fb96cf19034683572d1c89611
JonHalloran <angmar139@hotmail.com>
2019-03-11 16:37:46 -0700

    Update placeholder values

--- webpack_src/disability/Disability.jsx
+++ webpack_src/disability/Disability.jsx
@@ -4,6 +4,7 @@ class Disability extends React.Component{
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {};
   }
 
   handleSubmit(e){
@@ -24,42 +25,42 @@ class Disability extends React.Component{
       <form onSubmit={this.handleSubmit}>
         <label>
           Current Age
-          <input type="number" value={currentAge} placeholder='test' onChange={this.handleChange('currentAge')} />
+          <input type="number" value={currentAge} placeholder='Current Age' onChange={this.handleChange('currentAge')} />
         </label>
         <br />
         <label>
           Retirement Age
-          <input type="number" value={retirementAge} placeholder='test' onChange={this.handleChange('retirementAge')} />
+          <input type="number" value={retirementAge} placeholder='Retirement Age' onChange={this.handleChange('retirementAge')} />
         </label>
         <br />
         <label>
           Savings
-          <input type="number" value={currentSavings} placeholder='test' onChange={this.handleChange('currentSavings')} />
+          <input type="number" value={currentSavings} placeholder='Savings' onChange={this.handleChange('currentSavings')} />
         </label>
         <br />
         <label>
           Immediate Expenses
-          <input type="number" value={immediateExpenses} placeholder='test' onChange={this.handleChange('immediateExpenses')} />
+          <input type="number" value={immediateExpenses} placeholder='Immediate Expenses' onChange={this.handleChange('immediateExpenses')} />
         </label>
         <br />
         <label>
           Additional Annual Savings
-          <input type="number" value={additionalSavings} placeholder='test' onChange={this.handleChange('additionalSavings')} />
+          <input type="number" value={additionalSavings} placeholder='Additional Annual Savings' onChange={this.handleChange('additionalSavings')} />
         </label>
         <br />
         <label>
-          Monthly Expenses
-          <input type="number" value={monthlyExpensesAmount} placeholder='test' onChange={this.handleChange('monthlyExpensesAmount')} />
+          Additional Monthly Expenses
+          <input type="number" value={monthlyExpensesAmount} placeholder='Addional Monthly Expenses Amount' onChange={this.handleChange('monthlyExpensesAmount')} />
         </label>
         <br />
         <label>
           Monthly Expenses Start Year
-          <input type="number" value={monthlyExpensesStart} placeholder='test' onChange={this.handleChange('monthlyExpensesStart')} />
+          <input type="number" value={monthlyExpensesStart} placeholder='Monthly Expenses Start Year' onChange={this.handleChange('monthlyExpensesStart')} />
         </label>
         <br />
         <label>
           Monthly Expenses End Year
-          <input type="number" value={monthlyExpensesEnds} placeholder='test' onChange={this.handleChange('monthlyExpensesEnds')} />
+          <input type="number" value={monthlyExpensesEnds} placeholder='Monthly Expenses End Year' onChange={this.handleChange('monthlyExpensesEnds')} />
         </label>
         <br />
       </form>

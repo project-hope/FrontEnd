1adf2ecb38acbde6346bedf20c4b5df8f710bf19
JonHalloran <angmar139@hotmail.com>
2019-03-12 12:47:23 -0700

    Change everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
+/* eslint-disable filenames/match-regex, import/no-commonjs */
+
 const path = require('path');
+const context = path.resolve(__dirname, 'webpack_src');
 
 module.exports = {
-  context: __dirname,
-  entry: "./Root.js",
-  output: {
-    path: path.resolve(__dirname),
-    filename: "bundle.js"
-  },
-  // plugins: plugins,
+  context,
+  entry: './Root.js',
   module: {
-    rules: [
+    loaders: [{
+        include: path.resolve(__dirname, './webpack_src'),
+        loaders: [
+          'style-loader',
+          'css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ],
+        test: /\.css$/
+      },
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
+        include: path.resolve(__dirname, './webpack_src'),
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'env']
-        }
+          plugins: [
+            '@babel/transform-react-jsx',
+            [
+              'react-css-modules',
+              {
+                context
+              }
+            ]
+          ]
+        },
+        test: /\.(js|jsx)$/
       }
     ]
   },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
+  output: {
+    filename: '[name].js'
+  },
+  stats: 'minimal'
 };
